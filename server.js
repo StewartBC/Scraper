b54a4db65097c639c614e41a6c8451102852f424
StewartBC <stewcapps@gmail.com>
2018-07-31 23:15:23 -0400

    comments

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use(express.static("public"));
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
